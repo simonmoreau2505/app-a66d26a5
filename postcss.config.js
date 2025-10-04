02b4268f084971dd690ddc93910a712b69652d48
Simon Moreau <simon.moreau.2505@gmail.com>
2025-10-04 03:27:51 +0200

    Initial commit: Generated app code

---
+++ postcss.config.js
@@ -0,0 +1,7 @@
+export default {
+  // PostCSS configuration with Tailwind CSS and Autoprefixer
+  plugins: {
+    tailwindcss: {},
+    autoprefixer: {}
+  }
+};
---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+// Application root component
+const App: React.FC = () => {
+  return (
+    <main className="min-h-screen bg-white p-6">
+      {/* Display the required text in red */}
+      <p className="text-red-600">standard champion</p>
+    </main>
+  );
+};
+
+export default App;
